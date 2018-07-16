2e84d468fb627b136b4efcff1ea845ee3951cf83
Dmitrij Koniajev <dimchansky@gmail.com>
2018-07-16 11:46:58 +0300

    backend package added

---
+++ backend/backend.go
@@ -0,0 +1,206 @@
+package backend
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/contracts/chequebook"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// HandleNonceBackend internally handles nonce of the given addresses. It still calls PendingNonceAt of
+// inner backend, but returns PendingNonceAt as a maximum of pending nonce in block-chain and internally stored nonce.
+// It increments nonce for the given addresses after each successfully sent transaction (transaction may eventually
+// fail in block-cain).
+// Implementation is not thread-safe and should be used within one goroutine because otherwise invocations of
+// PendingNonceAt and SendTransaction should be done atomically to have sequence of nonce without gaps (so that
+// nonce would be equal to number of transactions sent).
+type HandleNonceBackend struct {
+	inner        chequebook.Backend
+	addressNonce map[common.Address]uint64
+}
+
+// NewHandleNonceBackend wraps backend and returns new instance of HandleNonceBackend.
+func NewHandleNonceBackend(inner chequebook.Backend, handleAddresses []common.Address) chequebook.Backend {
+	addressNonce := make(map[common.Address]uint64, len(handleAddresses))
+	for _, address := range handleAddresses {
+		addressNonce[address] = uint64(0)
+	}
+	b := &HandleNonceBackend{inner: inner, addressNonce: addressNonce}
+
+	if cr, ok := b.inner.(commiterRollbacker); ok {
+		return &simBackend{
+			b:  b,
+			cr: cr,
+		}
+	}
+
+	return b
+}
+
+// CodeAt returns the code of the given account. This is needed to differentiate
+// between contract internal errors and the local chain being out of sync.
+func (b *HandleNonceBackend) CodeAt(ctx context.Context, contract common.Address, blockNumber *big.Int) ([]byte, error) {
+	return b.inner.CodeAt(ctx, contract, blockNumber)
+}
+
+// CallContract executes an Ethereum contract call with the specified data as the input.
+func (b *HandleNonceBackend) CallContract(ctx context.Context, call ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
+	return b.inner.CallContract(ctx, call, blockNumber)
+}
+
+// PendingCodeAt returns the code of the given account in the pending state.
+func (b *HandleNonceBackend) PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error) {
+	return b.inner.PendingCodeAt(ctx, account)
+}
+
+// PendingNonceAt retrieves the current pending nonce associated with an account.
+func (b *HandleNonceBackend) PendingNonceAt(ctx context.Context, account common.Address) (nonce uint64, err error) {
+	nonce, err = b.inner.PendingNonceAt(ctx, account)
+	if err != nil {
+		return
+	}
+
+	innerNonce, shouldHandle := b.addressNonce[account]
+	if !shouldHandle {
+		return
+	}
+
+	if nonce > innerNonce {
+		b.addressNonce[account] = nonce
+	} else {
+		nonce = innerNonce
+	}
+
+	return
+}
+
+// SuggestGasPrice retrieves the currently suggested gas price to allow a timely
+// execution of a transaction.
+func (b *HandleNonceBackend) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
+	return b.inner.SuggestGasPrice(ctx)
+}
+
+// EstimateGas tries to estimate the gas needed to execute a specific
+// transaction based on the current pending state of the backend blockchain.
+// There is no guarantee that this is the true gas limit requirement as other
+// transactions may be added or removed by miners, but it should provide a basis
+// for setting a reasonable default.
+func (b *HandleNonceBackend) EstimateGas(ctx context.Context, call ethereum.CallMsg) (usedGas uint64, err error) {
+	return b.inner.EstimateGas(ctx, call)
+}
+
+// SendTransaction injects the transaction into the pending pool for execution.
+func (b *HandleNonceBackend) SendTransaction(ctx context.Context, tx *types.Transaction) (err error) {
+	err = b.inner.SendTransaction(ctx, tx)
+	if err != nil {
+		return
+	}
+
+	b.incrementNonce(tx)
+
+	return
+}
+
+func (b *HandleNonceBackend) incrementNonce(tx *types.Transaction) {
+	from, err := types.Sender(types.HomesteadSigner{}, tx)
+	if err != nil {
+		return // invalid sender
+	}
+
+	innerNonce, shouldHandle := b.addressNonce[from]
+	if !shouldHandle {
+		return
+	}
+
+	nonce := tx.Nonce() + 1
+	if nonce > innerNonce {
+		b.addressNonce[from] = nonce
+	}
+}
+
+// TransactionReceipt returns the receipt of a transaction by transaction hash.
+func (b *HandleNonceBackend) TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
+	return b.inner.TransactionReceipt(ctx, txHash)
+}
+
+// BalanceAt returns the balance of the account of given address.
+func (b *HandleNonceBackend) BalanceAt(ctx context.Context, address common.Address, blockNum *big.Int) (*big.Int, error) {
+	return b.inner.BalanceAt(ctx, address, blockNum)
+}
+
+// FilterLogs executes a log filter operation, blocking during execution and
+// returning all the results in one batch.
+func (b *HandleNonceBackend) FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error) {
+	return b.inner.FilterLogs(ctx, query)
+}
+
+// SubscribeFilterLogs creates a background log filtering operation, returning
+// a subscription immediately, which can be used to stream the found events.
+func (b *HandleNonceBackend) SubscribeFilterLogs(ctx context.Context, query ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error) {
+	return b.inner.SubscribeFilterLogs(ctx, query, ch)
+}
+
+type commiterRollbacker interface {
+	Commit()
+	Rollback()
+}
+
+type simBackend struct {
+	b  chequebook.Backend
+	cr commiterRollbacker
+}
+
+func (b *simBackend) CodeAt(ctx context.Context, contract common.Address, blockNumber *big.Int) ([]byte, error) {
+	return b.b.CodeAt(ctx, contract, blockNumber)
+}
+
+func (b *simBackend) CallContract(ctx context.Context, call ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
+	return b.b.CallContract(ctx, call, blockNumber)
+}
+
+func (b *simBackend) PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error) {
+	return b.b.PendingCodeAt(ctx, account)
+}
+
+func (b *simBackend) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
+	return b.b.PendingNonceAt(ctx, account)
+}
+
+func (b *simBackend) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
+	return b.b.SuggestGasPrice(ctx)
+}
+
+func (b *simBackend) EstimateGas(ctx context.Context, call ethereum.CallMsg) (usedGas uint64, err error) {
+	return b.b.EstimateGas(ctx, call)
+}
+
+func (b *simBackend) SendTransaction(ctx context.Context, tx *types.Transaction) error {
+	return b.b.SendTransaction(ctx, tx)
+}
+
+func (b *simBackend) TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
+	return b.b.TransactionReceipt(ctx, txHash)
+}
+
+func (b *simBackend) BalanceAt(ctx context.Context, address common.Address, blockNum *big.Int) (*big.Int, error) {
+	return b.b.BalanceAt(ctx, address, blockNum)
+}
+
+func (b *simBackend) FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error) {
+	return b.b.FilterLogs(ctx, query)
+}
+
+func (b *simBackend) SubscribeFilterLogs(ctx context.Context, query ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error) {
+	return b.b.SubscribeFilterLogs(ctx, query, ch)
+}
+
+func (b *simBackend) Commit() {
+	b.cr.Commit()
+}
+
+func (b *simBackend) Rollback() {
+	b.cr.Rollback()
+}
---
+++ backend/backend_test.go
@@ -0,0 +1,260 @@
+package backend
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"errors"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var (
+	handledAddressKey, _    = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+	nonHandledAddressKey, _ = crypto.HexToECDSA("8a1f9a8f95be41cd7ccb6168179afb4504aefe388d1e14474d32c45c72ce7b7a")
+	handledAddress          = crypto.PubkeyToAddress(handledAddressKey.PublicKey)
+	nonHandledAddress       = crypto.PubkeyToAddress(nonHandledAddressKey.PublicKey)
+)
+
+func TestHandleNonceBackend_PendingNonceAt(t *testing.T) {
+	t.Run("returns error for any address when inner backend returns error", func(t *testing.T) {
+		pendingNonceErr := errors.New("PendingNonceAt failed")
+		inner := &backendMock{PendingNonceAtFunc: func(ctx context.Context, account common.Address) (uint64, error) {
+			return 0, pendingNonceErr
+		}}
+		b := NewHandleNonceBackend(inner, []common.Address{handledAddress})
+		ctx := context.TODO()
+
+		_, err := b.PendingNonceAt(ctx, handledAddress)
+		if err != pendingNonceErr {
+			t.Errorf("expected error %v, but got %v", pendingNonceErr, err)
+		}
+
+		_, err = b.PendingNonceAt(ctx, nonHandledAddress)
+		if err != pendingNonceErr {
+			t.Errorf("expected error %v, but got %v", pendingNonceErr, err)
+		}
+	})
+
+	t.Run("returns latest maximum value of pending nonce for handled addresses", func(t *testing.T) {
+		maxNonce := uint64(12)
+		var innerNonce uint64
+		inner := &backendMock{PendingNonceAtFunc: func(ctx context.Context, account common.Address) (uint64, error) {
+			return innerNonce, nil
+		}}
+		b := NewHandleNonceBackend(inner, []common.Address{handledAddress})
+		ctx := context.TODO()
+
+		innerNonce = maxNonce
+		nonce, err := b.PendingNonceAt(ctx, handledAddress)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if nonce != innerNonce {
+			t.Errorf("expected nonce %v, but got %v", innerNonce, nonce)
+		}
+
+		innerNonce = maxNonce - 1
+		nonce, err = b.PendingNonceAt(ctx, handledAddress)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if nonce != maxNonce {
+			t.Errorf("expected nonce %v, but got %v", innerNonce, nonce)
+		}
+	})
+
+	t.Run("returns inner pending nonce for non-handled addresses", func(t *testing.T) {
+		maxNonce := uint64(12)
+		innerNonce := maxNonce
+		inner := &backendMock{PendingNonceAtFunc: func(ctx context.Context, account common.Address) (uint64, error) {
+			return innerNonce, nil
+		}}
+		b := NewHandleNonceBackend(inner, []common.Address{handledAddress})
+		ctx := context.TODO()
+
+		nonce, err := b.PendingNonceAt(ctx, nonHandledAddress)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if nonce != innerNonce {
+			t.Errorf("expected nonce %v, but got %v", innerNonce, nonce)
+		}
+
+		innerNonce = maxNonce - 1
+		nonce, err = b.PendingNonceAt(ctx, nonHandledAddress)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if nonce != innerNonce {
+			t.Errorf("expected nonce %v, but got %v", innerNonce, nonce)
+		}
+	})
+}
+
+func TestHandleNonceBackend_SendTransaction(t *testing.T) {
+	t.Run("increments pending nonce for handled address", func(t *testing.T) {
+		innerNonce := uint64(12)
+		sendTxNonce := uint64(50)
+		inner := &backendMock{
+			PendingNonceAtFunc: func(ctx context.Context, account common.Address) (uint64, error) {
+				return innerNonce, nil
+			},
+			SendTransactionFunc: func(ctx context.Context, tx *types.Transaction) error {
+				return nil
+			},
+		}
+		b := NewHandleNonceBackend(inner, []common.Address{handledAddress})
+		ctx := context.TODO()
+		tx := createTx(handledAddressKey, sendTxNonce, nonHandledAddress)
+
+		err := b.SendTransaction(ctx, tx)
+		if err != nil {
+			t.Fatalf("SendTransaction: %v", err)
+		}
+
+		nonce, err := b.PendingNonceAt(ctx, handledAddress)
+		if err != nil {
+			t.Fatalf("PendingNonceAt: %v", err)
+		}
+
+		if nonce != sendTxNonce+1 {
+			t.Errorf("expected pending nonce after tx sent is %v, but got %v", sendTxNonce+1, nonce)
+		}
+	})
+
+	t.Run("doesn't increment pending nonce for non-handled address", func(t *testing.T) {
+		innerNonce := uint64(12)
+		sendTxNonce := uint64(50)
+		inner := &backendMock{
+			PendingNonceAtFunc: func(ctx context.Context, account common.Address) (uint64, error) {
+				return innerNonce, nil
+			},
+			SendTransactionFunc: func(ctx context.Context, tx *types.Transaction) error {
+				return nil
+			},
+		}
+		b := NewHandleNonceBackend(inner, []common.Address{handledAddress})
+		ctx := context.TODO()
+		tx := createTx(nonHandledAddressKey, sendTxNonce, handledAddress)
+
+		err := b.SendTransaction(ctx, tx)
+		if err != nil {
+			t.Fatalf("SendTransaction: %v", err)
+		}
+
+		nonce, err := b.PendingNonceAt(ctx, nonHandledAddress)
+		if err != nil {
+			t.Fatalf("PendingNonceAt: %v", err)
+		}
+
+		if nonce != innerNonce {
+			t.Errorf("expected pending nonce after tx sent is %v, but got %v", innerNonce, nonce)
+		}
+	})
+
+	t.Run("doesn't increment pending nonce for handled address when transaction send failed", func(t *testing.T) {
+		innerNonce := uint64(12)
+		sendTxNonce := uint64(50)
+		sendTransactionErr := errors.New("SendTransaction failed")
+		inner := &backendMock{
+			PendingNonceAtFunc: func(ctx context.Context, account common.Address) (uint64, error) {
+				return innerNonce, nil
+			},
+			SendTransactionFunc: func(ctx context.Context, tx *types.Transaction) error {
+				return sendTransactionErr
+			},
+		}
+		b := NewHandleNonceBackend(inner, []common.Address{handledAddress})
+		ctx := context.TODO()
+		tx := createTx(handledAddressKey, sendTxNonce, nonHandledAddress)
+
+		err := b.SendTransaction(ctx, tx)
+		if err != sendTransactionErr {
+			t.Errorf("expected error %v, but got %v", sendTransactionErr, err)
+		}
+
+		nonce, err := b.PendingNonceAt(ctx, handledAddress)
+		if err != nil {
+			t.Fatalf("PendingNonceAt: %v", err)
+		}
+
+		if nonce != innerNonce {
+			t.Errorf("expected pending nonce after tx sent is %v, but got %v", sendTxNonce+1, nonce)
+		}
+	})
+}
+
+func createTx(key *ecdsa.PrivateKey, nonce uint64, to common.Address) *types.Transaction {
+	opts := bind.NewKeyedTransactor(key)
+	opts.Value = big.NewInt(1000000000000000000)
+	opts.GasLimit = uint64(21000)
+	opts.GasPrice = big.NewInt(1000000000)
+	rawTx := types.NewTransaction(nonce, to, opts.Value, opts.GasLimit, opts.GasPrice, nil)
+	tx, err := opts.Signer(types.HomesteadSigner{}, opts.From, rawTx)
+	if err != nil {
+		panic(err)
+	}
+	return tx
+}
+
+type backendMock struct {
+	CodeAtFunc             func(ctx context.Context, contract common.Address, blockNumber *big.Int) ([]byte, error)
+	CallContractFunc       func(ctx context.Context, call ethereum.CallMsg, blockNumber *big.Int) ([]byte, error)
+	PendingCodeAtFunc      func(ctx context.Context, account common.Address) ([]byte, error)
+	PendingNonceAtFunc     func(ctx context.Context, account common.Address) (uint64, error)
+	SuggestGasPriceFunc    func(ctx context.Context) (*big.Int, error)
+	EstimateGasFunc        func(ctx context.Context, call ethereum.CallMsg) (usedGas uint64, err error)
+	SendTransactionFunc    func(ctx context.Context, tx *types.Transaction) error
+	TransactionReceiptFunc func(ctx context.Context, txHash common.Hash) (*types.Receipt, error)
+	BalanceAtFunc          func(ctx context.Context, address common.Address, blockNum *big.Int) (*big.Int, error)
+}
+
+func (m *backendMock) CodeAt(ctx context.Context, contract common.Address, blockNumber *big.Int) ([]byte, error) {
+	return m.CodeAtFunc(ctx, contract, blockNumber)
+}
+
+func (m *backendMock) CallContract(ctx context.Context, call ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
+	return m.CallContractFunc(ctx, call, blockNumber)
+}
+
+func (m *backendMock) PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error) {
+	return m.PendingCodeAtFunc(ctx, account)
+}
+
+func (m *backendMock) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
+	return m.PendingNonceAtFunc(ctx, account)
+}
+
+func (m *backendMock) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
+	return m.SuggestGasPriceFunc(ctx)
+}
+
+func (m *backendMock) EstimateGas(ctx context.Context, call ethereum.CallMsg) (usedGas uint64, err error) {
+	return m.EstimateGasFunc(ctx, call)
+}
+
+func (m *backendMock) SendTransaction(ctx context.Context, tx *types.Transaction) error {
+	return m.SendTransactionFunc(ctx, tx)
+}
+
+func (m *backendMock) TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
+	return m.TransactionReceiptFunc(ctx, txHash)
+}
+
+func (m *backendMock) BalanceAt(ctx context.Context, address common.Address, blockNum *big.Int) (*big.Int, error) {
+	return m.BalanceAtFunc(ctx, address, blockNum)
+}
+
+func (m *backendMock) FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error) {
+	return nil, nil
+}
+
+func (m *backendMock) SubscribeFilterLogs(ctx context.Context, query ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error) {
+	return nil, nil
+}
